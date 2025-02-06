5744da614f6fb9acd2c5bb7d1881560f253f8690
adupoku71 <davipoku71@gmail.com>
2025-02-06 21:08:28 +0000

    refactored app to mirror the changes to connect db and blog model

--- app.js
+++ app.js
@@ -1,27 +1,13 @@
 import express from "express"
 const app = express()
 import cors from "cors"
-import mongoose from "mongoose"
-import { configDotenv } from "dotenv"
-configDotenv()
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
+import Blog from "./models/blog.js"
+import connect from "./utils/connectDb.js"
 
-const Blog = mongoose.model("Blog", blogSchema)
-
-const mongoUrl = process.env.MONGO_URI
-
-mongoose.connect(mongoUrl)
+connect()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.get("/", (req, res) => {
-  res.send("hello")
-})
 
 app.get("/api/blogs", (req, res) => {
   Blog.find({}).then((blogs) => {
