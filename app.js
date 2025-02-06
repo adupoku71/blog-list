81d9fa88310cda9da87af2b896b7f591ca82118d
adupoku71 <davipoku71@gmail.com>
2025-02-06 21:20:21 +0000

    added some comments to app.js

--- app.js
+++ app.js
@@ -1,26 +1,16 @@
 import express from "express"
 const app = express()
 import cors from "cors"
-import Blog from "./models/blog.js"
 import connect from "./utils/connectDb.js"
+import blogRouter from "./controllers/blogController.js"
 
+//db connection
 connect()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.get("/api/blogs", (req, res) => {
-  Blog.find({}).then((blogs) => {
-    res.status(200).json(blogs)
-  })
-})
-
-app.post("/api/blogs", (req, res) => {
-  const blog = new Blog(req.body)
-
-  blog.save().then((result) => {
-    res.status(201).json(result)
-  })
-})
+//handles blog request logic
+app.use("/api/blogs", blogRouter)
 
 export default app
