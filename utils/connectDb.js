97e30f98438e1b062e3eb5d986fd2859d537f770
adupoku71 <davipoku71@gmail.com>
2025-03-03 21:09:54 +0000

    made some changes to config and connect db

--- utils/connectDb.js
+++ utils/connectDb.js
@@ -1,13 +1,13 @@
 import mongoose from "mongoose"
 import { MONGO_URI } from "./config.js"
 
-console.log("connecting to", MONGO_URI)
+console.log("connecting to database")
 
 const connect = () => {
   mongoose
     .connect(MONGO_URI)
     .then(() => {
-      console.log("connected to", MONGO_URI, "sucessfully")
+      console.log("connected sucessfully")
     })
     .catch((err) => {
       console.log(err.message, "failed to connect")
