f3dedc628cc109cf534f9493ff8dfe7d32516859
adupoku71 <davipoku71@gmail.com>
2025-03-07 17:21:13 +0000

    test for id property of a blog

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -15,6 +15,12 @@ test("blog returns correct amount of blog posts", async () => {
   assert.strictEqual(response.body.length, 6)
 })
 
+test("unique identifier property of the blog posts is named id", async () => {
+  const blogs = await api.get("/api/blogs")
+  const blog = blogs.body[0]
+  assert.ok(blog.id, "id field is missing")
+  assert.equal(blog._id, undefined, "_id field should not exist")
+})
 after(async () => {
   await mongoose.connection.close()
 })
